2a47e1a14b3616346ad645558642031ed22437b2
JSM <JSM@DESKTOP-E4NR1II>
2019-05-14 15:37:48 +0100

    Alteração

--- src/lsystem2/LSystem2.java
+++ src/lsystem2/LSystem2.java
@@ -16,6 +16,7 @@ public class LSystem2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Olá mundo");
     }
     
 }
